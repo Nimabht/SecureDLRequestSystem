1cdc77ee1896de63324c9fd8ee60895f95a41460
Nima <nima@gardoon.io>
2024-08-12 02:05:02 +0330

    feat: Add UsersModule for managing user-related functionality

---
+++ backend/src/user/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/Model/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOneByUsername(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async findOneById(id: number): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { userId: id } });
+  }
+
+  async findOneByGoogleId(googleId: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { googleId } });
+  }
+
+  async createUserWithGoogle(
+    googleId: string,
+    email: string,
+    firstName: string,
+    lastName: string,
+  ): Promise<User> {
+    const newUser = this.usersRepository.create({
+      googleId,
+      email,
+      username: `${firstName} ${lastName}`,
+    });
+    return this.usersRepository.save(newUser);
+  }
+
+  async createUser(
+    username: string,
+    password: string,
+    email?: string,
+  ): Promise<User> {
+    const newUser = this.usersRepository.create({ username, password, email });
+    return this.usersRepository.save(newUser);
+  }
+}
