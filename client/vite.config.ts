7e3025163627981718e9bc300bdbd53438759a37
Nima <nima@gardoon.io>
2024-08-12 21:44:40 +0330

    feat: https protocol added to application

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 
-// https://vitejs.dev/config/
+process.env.VITE_HTTPS_ENABLED = "true";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https:
+      process.env.VITE_HTTPS_ENABLED === "true"
+        ? {
+            key: fs.readFileSync(
+              path.resolve(
+                __dirname,
+                process.env.SSL_KEY_PATH || "../secrets/key.pem",
+              ),
+            ),
+            cert: fs.readFileSync(
+              path.resolve(
+                __dirname,
+                process.env.SSL_KEY_PATH || "../secrets/cert.pem",
+              ),
+            ),
+          }
+        : undefined,
+  },
+});
